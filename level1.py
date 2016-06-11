ba6a2aac9e1bc76ca137315303c24f07ea23960a
Wei Ren <renwei@smartconn.cc>
2016-06-10 20:16:36 -0700

    Use maketrans and str.translate() in level1.
    Change-Id: Id059d421b77c3fd68563d1b2237c706c900f6c20
    Signed-off-by: Wei Ren <renwei@smartconn.cc>

--- level1.py
+++ level1.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python2.7
+from string import maketrans
 
 # http://www.pythonchallenge.com/pc/def/map.html
 
-def rotateLetter(ch):
-    if ch.isalpha():
-        if ch.islower():
-            return chr((ord(ch) - ord('a') + 2) % 26 + ord('a'))
-        else:
-            return chr((ord(ch) - ord('A') + 2) % 26 + ord('A'))
-    else:
-        return ch
+intab = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+outtab = "cdefghijklmnopqrstuvwxyzabCDEFGHIJKLMNOPQRSTUVWXYZAB"
+transTab = maketrans(intab, outtab)
 
 scrambledStr = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
-print ''.join(rotateLetter(ch) for ch in scrambledStr)    
+print scrambledStr.translate(transTab)   
 
 url = 'map'
-
-print ''.join(rotateLetter(ch) for ch in url)
+print url.translate(transTab)
